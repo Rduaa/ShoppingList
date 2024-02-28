38a335d061467a23507a7cb35b7ec2ed0cab95fa
Ruslan <example@gmail.com>
2024-02-28 10:05:01 +0300

    screen layouts + ViewHolder and ShopListAdapter

---
+++ app/src/main/java/com/rdua/shoppinglist/presentation/ShopListAdapter.kt
@@ -0,0 +1,44 @@
+package com.rdua.shoppinglist.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.rdua.shoppinglist.R
+import com.rdua.shoppinglist.domain.ShopItem
+
+class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder> () {
+
+    val list = listOf<ShopItem>()
+
+    override fun equals(other: Any?): Boolean {
+        return super.equals(other)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled, parent, false)
+        return ShopItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
+        val shopItem = list[position]
+        viewHolder.tvName.text = shopItem.name
+        viewHolder.tvCount.text = shopItem.count.toString()
+        viewHolder.view.setOnLongClickListener{
+            true
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val tvName = view.findViewById<TextView>(R.id.tv_name)
+        val tvCount = view.findViewById<TextView>(R.id.tv_count)
+    }
+
+
+}
