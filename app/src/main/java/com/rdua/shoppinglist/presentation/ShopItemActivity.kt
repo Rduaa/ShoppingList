226b26a38175f3d77c15e359d496f9ae4b45ea96
Ruslan <example@gmail.com>
2024-03-26 10:38:36 +0300

    SecondScreenStart, ShopItemActivity, some changes in manifest

---
+++ app/src/main/java/com/rdua/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.rdua.shoppinglist.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rdua.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
